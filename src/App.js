a374de1b9efdc617d6d37da9f17b1c4f65ca7486
DistantGabe <gabrielbraida@hotmail.com>
2019-06-12 17:16:43 -0300

    coomit Silvia

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       		 Biska	! 
+		Silvia, Silvia, Pam Pam Pam Pam Pam Pam... Silvia
         </p>
         <a
           className="App-link"
